e40a4c280bb102b0cc213b4ad3208cbfa2f15cee
gabrielcon6 <gabrielcon6cao@gmail.com>
2021-09-25 11:50:49 +1000

    game skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import LangGame from "./LangGame.js";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-body">
+        <LangGame></LangGame>
+      </div>
+    </div>
+  );
+}
+
+export default App;
