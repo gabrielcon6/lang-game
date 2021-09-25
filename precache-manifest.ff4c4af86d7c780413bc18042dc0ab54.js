ffb06ba6fb1b6dfdc2c50fc0e07ba608d8de637a
gabrielcon6 <gabrielcon6cao@gmail.com>
2021-09-25 11:55:39 +1000

    Updates

---
+++ precache-manifest.ff4c4af86d7c780413bc18042dc0ab54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e2bd6c48170aa7cf99f28fba053dbf",
+    "url": "/lang-game/index.html"
+  },
+  {
+    "revision": "3312df2b375555efdf7b",
+    "url": "/lang-game/static/css/main.41f5a95f.chunk.css"
+  },
+  {
+    "revision": "2c5282764d462c7a4e40",
+    "url": "/lang-game/static/js/2.32ff6e13.chunk.js"
+  },
+  {
+    "revision": "3312df2b375555efdf7b",
+    "url": "/lang-game/static/js/main.5d7a428c.chunk.js"
+  },
+  {
+    "revision": "f8bded3a2d57fec57f11",
+    "url": "/lang-game/static/js/runtime~main.b5903721.js"
+  },
+  {
+    "revision": "68ee077cb12ba5f3da96981820a79b36",
+    "url": "/lang-game/static/media/deck.68ee077c.png"
+  },
+  {
+    "revision": "8c0371b22940e53cc34af8d5997b4166",
+    "url": "/lang-game/static/media/lang-logo.8c0371b2.png"
+  }
+]);
